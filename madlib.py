716cab6bb15be12dc4d4d6fa6cf218e44cfd5e36
Salim-Shaik <88785066+Salim-Shaik@users.noreply.github.com>
2022-03-17 22:44:23 +0200

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,15 @@
+youtuber = ""
+
+
+print("subscribe to youtuber"+ youtuber)
+print("subscribe to youtuber{}".format(youtuber))
+print(f"subscribe to youtuber{youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("verb")
+verb2 = input("verb")
+famous_person = input("famous person:")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+print(madlib)
